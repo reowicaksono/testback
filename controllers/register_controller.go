95bb868890831a1f33c3816014d514d4b0462fc8
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-07-06 10:25:32 +0700

    feat: regiser controller user

---
+++ controllers/register_controller.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"net/http"
+	"testback/database"
+	"testback/helpers"
+	"testback/models"
+	"testback/structs"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	var req = structs.UserCreateRequest{}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, structs.ErrorResponse{
+			Success: false,
+			Message: "Validasi Errors",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	user := models.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Username: req.Username,
+		Password: helpers.HasPassword(req.Password),
+	}
+
+	if err := database.DB.Create(&user).Error; err != nil {
+		if helpers.IsDuplicateEntryError(err) {
+			c.JSON(http.StatusConflict, structs.ErrorResponse{
+				Success: false,
+				Message: "Duplicate Any Error",
+				Errors:  helpers.TranslateErrorMessage(err),
+			})
+		} else {
+			c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+				Success: false,
+				Message: "Failed to Create User",
+				Errors:  helpers.TranslateErrorMessage(err),
+			})
+		}
+		return
+	}
+
+	c.JSON(http.StatusCreated, structs.SuccessResponse{
+		Success: true,
+		Message: "User Create Successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+
+}
