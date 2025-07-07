4c6651c34447f536195ffedf0d28d89f9502ee2c
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-07-07 09:13:11 +0700

    add: cors configuration:

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,20 @@ import (
 	"testback/controllers"
 	"testback/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	// cors
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "PUT", "POST", "DELETE"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 
 	// auth
 	router.POST("/api/register", controllers.Register)
